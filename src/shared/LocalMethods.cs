9632d0034f06af0aea8272156741920b594676b1
Julio de Leon <julio.deleon@redcoresolutions.com>
2024-05-07 10:19:21 +0800

    feat: with C# code

---
+++ src/shared/LocalMethods.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using EmailSender_Outlook;
+
+namespace QuickStart
+{
+    public class LocalMethods
+    {
+        public async Task<object> CreateOutlook(dynamic input)
+        {
+            var toEmail = input.toEmail;
+            var body = input.body;
+            var subject = input.subject;
+            var ccEmail = input.ccEmail;
+            var bccEmail = input.bccEmail;
+            var attachment = input.attachment;
+            
+            Console.WriteLine(toEmail);
+            Console.WriteLine(body);
+            Console.WriteLine(subject);
+            Console.WriteLine(ccEmail);
+            Console.WriteLine(attachment);
+            Console.WriteLine(bccEmail);
+
+            List<string> filePaths = new List<string>
+            {
+                attachment,
+            };
+
+            var emailSender = new EmailSender();
+            emailSender.SendEmail(subject, body, toEmail, ccEmail, bccEmail, filePaths);
+
+            return "CREATING EMAIL...";
+        }
+    }
+}
